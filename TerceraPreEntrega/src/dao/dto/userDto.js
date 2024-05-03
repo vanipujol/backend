0fd99ee0e5815f168f2cea81d34e66a1df795eda
vanipujol <vanicba@gmail.com>
2024-05-03 14:13:15 -0300

    final project

--- TerceraPreEntrega/src/dao/dto/userDto.js
+++
@@ -1,9 +0,0 @@
-export class GetUserDTO{
-    constructor(user) {
-        this.full_name = `${user.first_name} ${user.last_name}`;
-        this.email = `${user.email}`;
-        this.age = `${user.age}`;
-        this.role = `${user.role}`;
-        this.cart = `${user.cart._id}`;
-    }
-}
